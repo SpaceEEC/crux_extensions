f38efe2e7b32d1b8c574c29491a52330d8ddf683
SpaceEEC <spaceeec@yahoo.com>
2019-05-31 17:07:14 +0200

    feat(Command): add opts arg to handler callbacks

--- lib/extensions/command/handler.ex
+++ lib/extensions/command/handler.ex
@@ -1,4 +1,4 @@
-defmodule Crux.Extensions.Command.Handler do
+defmodule Crux.Extensions.Handler do
   @moduledoc """
     Handler module serving as entry point for command pipelines.
 
@@ -8,141 +8,148 @@ defmodule Crux.Extensions.Command.Handler do
     defmodule MyBot.Handler do
       use Crux.Extensions.Command.Handler
 
-      def commands() do
+      def commands(_opts) do
         [
           MyBot.Command.Ping,
           MyBot.Command.Dog
         ]
       end
-      def producers(), do: MyBot.Producers.fetch()
-      def rest(), do: MyBot.Rest
-      def prefixes(), do: ["!"]
+      def producers(_opts), do: MyBot.Producers.fetch()
+      def rest(_opts), do: MyBot.Rest
+      def prefixes(_opts), do: ["!"]
     end
     ```
   """
-
-  alias Crux.Extensions.Command
+  alias Crux.Base.Processor
   alias Crux.Structs.Message
 
-  defmacro __using__(_ \\ []) do
-    quote location: :keep do
-      @doc false
-      def start_link(arg), do: Crux.Extensions.Command.Supervisor.start_link(__MODULE__, arg)
-      @doc false
-      def child_spec(arg), do: Crux.Extensions.Command.Supervisor.child_spec(__MODULE__, arg)
+  alias Crux.Extensions.Command
+  alias Crux.Extensions.Command.Supervisor
 
-      @doc """
-        The default `c:#{unquote(__MODULE__)}.respond/1` implementation, sending a
-        message with the in `Command.t()` defined `response` to the defined `response_channel`.
-      """
-      @spec respond(Command.t()) :: Message.t() | no_return()
-      def respond(command) do
-        unquote(__MODULE__).respond(command, __MODULE__)
-      end
-
-      @doc """
-        The default `c:#{unquote(__MODULE__)}.handle_prefixes/1` implementation, fetching
-        all prefixes from `c:#{unquote(__MODULE__)}.prefix/0` and try to slice them off.
-      """
-      @spec handle_prefixes(message :: Message.t()) :: [{:ok, String.t()} | :error]
-      def handle_prefixes(message) do
-        unquote(__MODULE__).handle_prefixes(message, __MODULE__)
-      end
-
-      @doc """
-        TThe default `c:#{unquote(__MODULE__)}.match_commands/3` implementation, fetching
-        all commands from `c:#{unquote(__MODULE__)}.commands/0` and comparing their triggers
-        with the beginning of `content`.
-
-        `content` is the string `c:#{unquote(__MODULE__)}.handle_prefixes/1` returned.
-      """
-      @spec match_commands(
-              content :: String.t(),
-              Message.t(),
-              shard_id :: non_neg_integer()
-            ) :: [{Command.command_info(), Command.t()}]
-      def match_commands(content, message, shard_id) do
-        unquote(__MODULE__).match_commands(content, message, shard_id, __MODULE__)
-      end
-    end
-  end
-
-  ### Required Callbacks
-
-  @doc """
-    Gets all available commands.
+  @typedoc """
+    Custom options provided by the developer.
   """
-  @callback commands() :: [Command.command()]
+  @type opts :: term()
 
   @doc """
     Gets the current producers.
   """
-  @callback producers() :: [pid()]
+  @callback producers(opts()) :: [pid()]
+  @doc """
+    Matches an event, returning a message, shard id tuple on success, or :error on error.
+  """
+  @callback match_event(
+              opts(),
+              event :: Processor.event()
+            ) :: {:ok, {Message.t(), shard_id :: non_neg_integer()}} | :error
+
+  @doc """
+    Gets the current prefixes
+  """
+  @callback prefixes(opts) :: [String.t() | nil]
+  @doc """
+    Matches prefixes, slicing them off the message' content and returning an :ok, content tuple, otherwise :error
+  """
+  @callback match_prefixes(
+              opts(),
+              message :: Message.t()
+            ) :: [{:ok, String.t()} | :error]
+
+  @doc """
+    Gets all available commands.
+  """
+  @callback commands(opts()) :: [Command.command()]
+  @doc """
+    Matches all command given the content.
+  """
+  @callback match_commands(
+              opts(),
+              content :: String.t(),
+              message :: Message.t(),
+              shard_id :: non_neg_integer()
+            ) :: [{Command.command_info(), Command.t()}]
 
   @doc """
     Gets the module handling rest.
 
     > See `Crux.Rest`
   """
-  @callback rest() :: module()
-
+  @callback rest(opts()) :: module()
   @doc """
-    Gets the current prefixes.
-
-    > `nil` is used for "no prefix"
+    Called after the command is done to send the response.
   """
-  @callback prefixes() :: [String.t() | nil]
-
-  ### Optional callbacks
+  @callback respond(opts(), Command.t()) :: any()
 
   @doc """
     Called when an error occured.
 
     > Not implementing this function causes any errors to terminate the handling process.
   """
-  @callback on_error(Command.t(), todo :: any()) :: any()
+  @callback on_error(
+              opts(),
+              Command.t(),
+              todo :: any()
+            ) :: any()
 
-  @doc """
-    Called after the command is done to send the response.
-  """
-  @callback respond(Command.t()) :: any()
+  @optional_callbacks match_event: 2,
+                      match_prefixes: 2,
+                      match_commands: 4,
+                      respond: 2,
+                      on_error: 3
 
-  @doc """
-    Removes the prefix from content.
+  defmacro __using__(_opts \\ []) do
+    quote location: :keep do
+      @behaviour unquote(__MODULE__)
 
-    Returns a list of:
-    - `{:ok, content}` if a prefix matched
-    - `:error` if a prefix did not
-  """
-  @callback handle_prefixes(message :: Message.t()) :: [{:ok, String.t()} | :error]
+      ### Default and static behavior
+      @doc false
+      def start_link(arg), do: Supervisor.start_link(__MODULE__, arg)
+      @doc false
+      def child_spec(arg), do: Supervisor.child_spec(__MODULE__, arg)
 
-  @doc """
-    Gets all commands matching the given content.
-  """
-  @callback match_commands(
-              content :: String.t(),
-              message :: Message.t(),
-              shard_id :: non_neg_integer()
-            ) :: [{Command.command_info(), Command.t()}]
+      @doc false
+      def start_task(opts, event) do
+        with {:ok, data} <- match_event(opts, event) do
+          Task.start_link(unquote(__MODULE__), :handle_event, [__MODULE__, opts, data])
+        else
+          _ ->
+            :ignore
+        end
+      end
 
-  @optional_callbacks respond: 1, handle_prefixes: 1, match_commands: 3
+      ### Overridables
+      @doc "Default implementation matching only MESSAGE_CREATE events"
+      defdelegate match_event(opts, event), to: unquote(__MODULE__)
 
-  ### Functions always here
+      @doc "Default implementation using `prefixes/1` and stripping them off the beginning of the message."
+      def match_prefixes(opts, message),
+        do: unquote(__MODULE__).match_prefixes(__MODULE__, opts, message)
+
+      @doc "Default implementation splitting arguments by spaces."
+      def match_commands(opts, content, message, shard_id),
+        do: unquote(__MODULE__).match_commands(__MODULE__, opts, content, message, shard_id)
+
+      @doc "Default implementation using `rest/1` and `c:Crux.Rest.create_message!/2`."
+      def respond(opts, command), do: unquote(__MODULE__).respond(__MODULE__, opts, command)
+
+      defoverridable handle?: 2, match_prefixes: 2, match_commands: 4, respond: 2
+    end
+  end
+
+  ### Default and static behavior
 
   @doc false
-  # Handles incoming events, extracting content, matching commands, running them, and handling errors if necessary.
-  @spec handle_event(Crux.Base.Consumer.event(), module()) :: any()
-  def handle_event({_, message, shard_id}, module) do
-    for {:ok, content} <- module.handle_prefixes(message),
-        {command_info, command} <-
-          module.match_commands(content, message, shard_id) do
+  # Handles incoming messages, extracting their content, matching commands, running them, and handling errors if necessary.
+  def handle_event(handler_module, opts, {message, shard_id}) do
+    for {:ok, content} <- handler_module.match_prefixes(opts, message),
+        {command_info, command} <- handler_module.match_commands(opts, content, message, shard_id) do
       command =
-        if function_exported?(module, :on_error, 3) do
+        if function_exported?(handler_module, :on_error, 3) do
           try do
             run_command(command_info, command)
           rescue
             e ->
-              module.on_error(command, e, __STACKTRACE__)
+              handler_module.on_error(command, e, __STACKTRACE__)
 
               nil
           end
@@ -151,7 +158,7 @@ defmodule Crux.Extensions.Command.Handler do
         end
 
       if command do
-        module.respond(command)
+        handler_module.response(opts, command)
       end
     end
   end
@@ -159,13 +166,13 @@ defmodule Crux.Extensions.Command.Handler do
   @doc false
   # Executes a command, including all required commands (and their required commands).
   @spec run_command(Command.command(), Command.t()) :: Command.t()
-  def run_command(_command, %Command{halted: true} = command), do: command
+  defp run_command(_command, %Command{halted: true} = command), do: command
 
-  def run_command(command_mod, %Command{} = command) when is_atom(command_mod) do
+  defp run_command(command_mod, %Command{} = command) when is_atom(command_mod) do
     run_command({command_mod, []}, command)
   end
 
-  def run_command({command_mod, command_arg}, %Command{} = command) do
+  defp run_command({command_mod, command_arg}, %Command{} = command) do
     if function_exported?(command_mod, :required, 0) do
       Enum.reduce(command_mod.required(), command, &run_command/2)
     else
@@ -180,46 +187,52 @@ defmodule Crux.Extensions.Command.Handler do
     end
   end
 
-  ### Default implementation for optional callbacks
+  ### Default and static behavior -- End
+
+  ### Optional Callbacks Default Implementation
 
   @doc false
-  # Responds with a message if requested.
-  @spec respond(Command.t(), module()) :: Message.t() | nil | no_return()
-  def respond(%Command{response: response, response_channel: response_channel}, _module)
-      when is_nil(response)
-      when is_nil(response_channel) do
-    nil
+  # Matches only MESSAGE_CREATE events
+  def match_event(_opts, {:MESSAGE_CREATE, message, shard_id}) do
+    {:ok, {message, shard_id}}
   end
 
-  def respond(%Command{response: response, response_channel: response_channel}, module) do
-    module.rest().create_message!(response_channel, response)
+  def match_event(_opts, _event), do: :error
+
+  @doc false
+  # Slices off the prefix(es) from the content
+  def match_prefixes(handler_module, opts, %{content: content}) do
+    content_down = String.downcase(content)
+
+    opts
+    |> handler_module.prefixes()
+    |> Enum.map(fn
+      nil ->
+        {:ok, content}
+
+      prefix ->
+        if String.starts_with?(content_down, prefix) do
+          content = String.slice(content, String.length(prefix)..-1)
+
+          {:ok, content}
+        else
+          :error
+        end
+    end)
   end
 
   @doc false
-  # Trims the prefixes off the message's content.
-  @spec handle_prefixes(Message.t(), module()) :: [{:ok, String.t()} | :error]
-  def handle_prefixes(%Message{content: content}, module) do
-    prefixes = module.prefixes()
-
-    Enum.map(prefixes, &handle_prefix(content, &1))
-  end
-
-  @doc false
-  # Matches commands given the content and returns a list of command infos and `Command.t()`s.
-  @spec match_commands(
-          content :: String.t(),
-          Message.t(),
-          shard_id :: non_neg_integer(),
-          module()
-        ) :: [{Command.command_info(), Command.t()}]
-  def match_commands(content, message, shard_id, module) do
-    [command | args] = String.split(content, ~r{ +})
+  def match_commands(handler_module, opts, content, message, shard_id) do
+    [command | args] =
+      content
+      |> String.trim_leading()
+      |> String.split(~r{ +})
 
     command = String.downcase(command)
 
     command_infos =
       Enum.map(
-        module.commands(),
+        handler_module.commands(opts),
         fn
           {_command_mod, _command_opts} = command_info -> command_info
           command_mod when is_atom(command_mod) -> {command_mod, []}
@@ -239,22 +252,23 @@ defmodule Crux.Extensions.Command.Handler do
     end
   end
 
-  ### Helpers
-
-  # Slices the prefix off the content and returns {:ok, rest}, if not possible, returns :error.
-  @spec handle_prefix(content :: String.t(), prefix :: String.t() | nil) ::
-          {:ok, String.t()} | :error
-  defp handle_prefix(content, nil), do: {:ok, content}
-
-  defp handle_prefix(content, prefix) do
-    content_down = String.downcase(content)
-
-    if String.starts_with?(content_down, prefix) do
-      content = String.slice(content, String.length(prefix)..-1)
-
-      {:ok, content}
-    else
-      :error
-    end
+  @doc false
+  # Sends a message, if appropriate
+  def respond(_handler_module, _opts, %Command{
+        response: response,
+        response_channel: response_channel
+      })
+      when is_nil(response)
+      when is_nil(response_channel) do
+    nil
   end
+
+  def respond(handler_module, opts, %Command{
+        response: response,
+        response_channel: response_channel
+      }) do
+    handler_module.rest(opts).create_message!(response_channel, response)
+  end
+
+  ### Optional Callbacks Default Implementation -- End
 end
--- lib/extensions/command/supervisor.ex
+++ lib/extensions/command/supervisor.ex
@@ -3,26 +3,37 @@ defmodule Crux.Extensions.Command.Supervisor do
 
   use ConsumerSupervisor
 
-  alias Crux.Extensions.Command.Consumer
-
-  def child_spec(handler, opts) when is_atom(handler) and is_list(opts) do
+  def child_spec(handler_module, opts)
+      when is_atom(handler_module) and is_list(opts) do
     %{
       id: __MODULE__,
-      start: {__MODULE__, :start_link, [handler, opts]},
+      start: {__MODULE__, :start_link, [handler_module, opts]},
       type: :supervisor
     }
   end
 
-  @spec start_link(handler :: module(), [GenServer.option()]) :: Supervisor.on_start()
-  def start_link(handler, opts) when is_atom(handler) do
-    ConsumerSupervisor.start_link(__MODULE__, handler, opts)
+  @spec start_link(
+          handler_module :: module(),
+          [GenServer.option()]
+        ) :: Supervisor.on_start()
+  def start_link(handler_module, opts)
+      when is_atom(handler_module) do
+    ConsumerSupervisor.start_link(__MODULE__, {handler_module, opts}, opts)
   end
 
-  @spec init(handler :: module()) ::
+  @spec init({handler :: module(), opts :: term()}) ::
           {:ok, [:supervisor.child_spec()], options :: keyword()} | :ignore
-  def init(handler) do
-    children = [Consumer.child_spec(handler)]
-    opts = [strategy: :one_for_one, subscribe_to: handler.producers()]
+  def init({handler, opts})
+      when is_atom(handler) do
+    children = [
+      %{
+        id: handler,
+        start: {handler, :start_task, []},
+        type: :worker
+      }
+    ]
+
+    opts = [strategy: :one_for_one, subscribe_to: handler.producers(opts)]
 
     ConsumerSupervisor.init(children, opts)
   end
